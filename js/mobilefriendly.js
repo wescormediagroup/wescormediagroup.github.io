fba2aa9e24170d0c81d94e67439781b91e0a1a64
WesCor Media Group <parhamshawx3@proton.me>
2024-02-06 12:31:52 -0500

    updated mobile js

--- js/mobilefriendly.js
+++ js/mobilefriendly.js
@@ -1,20 +1,24 @@
 $(document).ready(function() {
-    function toggleDropdown() {
-      $(".dropdown").toggleClass("open");
-    }
+  function toggleDropdown() {
+    $(".dropdown").toggleClass("open");
+  }
 
-    var isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+  var isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
 
-    if (isTouchDevice) {
-      $(".dropdown-toggle").on("click", function(e) {
-        e.preventDefault();
-        toggleDropdown();
-      });
+  if (isTouchDevice) {
+    $(".dropdown-toggle").on("click", function(e) {
+      e.preventDefault();
+      toggleDropdown();
+    });
 
-      $(document).on("click", function(e) {
-        if (!$(e.target).closest(".dropdown").length && !$(e.target).closest(".dropdown-toggle").length) {
-          $(".dropdown").removeClass("open");
-        }
-      });
-    }
-  });
+    $(".dropdown-menu a").click(function(e) {
+      e.stopPropagation();
+    });
+
+    $(document).on("click", function(e) {
+      if (!$(e.target).closest(".dropdown").length && !$(e.target).closest(".dropdown-toggle").length) {
+        $(".dropdown").removeClass("open");
+      }
+    });
+  }
+});
