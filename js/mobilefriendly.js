0c218213499da0ee56294c4e9120eabdb214d667
wescormediagroup <155579598+wescormediagroup@users.noreply.github.com>
2024-02-06 11:19:19 -0500

    Add files via upload

---
+++ js/mobilefriendly.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    function toggleDropdown() {
+      $(".dropdown").toggleClass("open");
+    }
+
+    var isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+
+    if (isTouchDevice) {
+      $(".dropdown-toggle").on("click", function(e) {
+        e.preventDefault();
+        toggleDropdown();
+      });
+
+      $(document).on("click", function(e) {
+        if (!$(e.target).closest(".dropdown").length && !$(e.target).closest(".dropdown-toggle").length) {
+          $(".dropdown").removeClass("open");
+        }
+      });
+    }
+  });
